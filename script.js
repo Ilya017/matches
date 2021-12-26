a4e5777bd07a66df91d2bd1b9b26d8230fa74fcf
Ilya <ilya.komar017@gmail.com>
2021-12-26 18:39:20 +0200

    last

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-let n = prompt("Задайте кол-во спичек:");
+let n;
+function game(num) {
+num = prompt("Задайте кол-во спичек:");
 let str = " ";
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < num; i++) {
   str += " | ";
 }
-alert(n + str);
-while (n > 0) {
+alert(num + str);
+while (num > 0) {
   alert("Your turn!");
   let m = prompt("Get the matches!");
   while (isNaN(m) || m < 0 || m > 3 || m == 0) {
     alert("Error");
     m = prompt("Get the matches one more time");
   }
-  n -= m;
+  num -= m;
+  if (num < 0) {
+    num = 0;
+  }
   let str = " ";
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < num; i++) {
     str += "| ";
   }
-  alert(n + str);
-  if (n == 0) {
+  alert(num + str);
+  if (num == 0) {
     alert("you win!");
-    break;
+    return;
   }
   alert("my turn!");
   let rand = Math.floor(Math.random() * 3) + 1;
-  n -= rand;
+  num -= rand;
+  if (num < 0) {
+    num = 0;
+  }
   str = " ";
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < num; i++) {
     str += "| ";
   }
-  alert(n + str);
+  alert(num + str);
+  if (num == 0) {
+    alert("I win");
+    return;
+  }
 }
+}
+game(n);
